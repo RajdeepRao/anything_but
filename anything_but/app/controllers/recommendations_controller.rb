a6e2ac167888a2fb8982de85052b1558a60f8f1b
Mahati Gollamudi <github email address>
2016-09-08 18:24:00 -0400

    Create recs controller actions that will interact with ajax

--- anything_but/app/controllers/recommendations_controller.rb
+++ anything_but/app/controllers/recommendations_controller.rb
@@ -1,3 +1,13 @@
 class RecommendationsController < ApplicationController
 
+  def create
+    #TROY WILL FIGURE OUT HOW THE REQUEST INTERACTS WITH THIS
+    #AND SHARE WHAT THIS WILL HAVE TO BE STRUCTURED
+  end
+
+  def show
+
+  end
+
+
 end
