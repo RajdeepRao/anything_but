c20cbd5b57e5994057de46e6199633ac5a82c249
Troy Chryssos <github email address>
2016-09-08 18:22:59 -0400

    added routes for recommendation

--- anything_but/config/routes.rb
+++ anything_but/config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
 
   resources :users
 
+  resources :recommendations, only: [:create, :show]
+
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
 
   get '/signup', to: 'users#new'
   post '/users', to: 'users#create'
-  
+
 end
