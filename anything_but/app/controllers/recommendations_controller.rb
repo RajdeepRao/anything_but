d73289e93c9b1042a59ec9627a579dab0e7853cd
Troy Chryssos <github email address>
2016-09-08 20:11:50 -0400

    created ajax request to recommendations controller changed partial for 'recommendations' from an actual form_for to an arbitrary form from which the ajax request pulls the value and passes through to the recommendations controller via the create path along with the latitude and longitude. I added a dumb little alert that pops up to confirm that this request was successful.

--- anything_but/app/controllers/recommendations_controller.rb
+++ anything_but/app/controllers/recommendations_controller.rb
@@ -1,13 +1,11 @@
 class RecommendationsController < ApplicationController
 
   def create
-    #TROY WILL FIGURE OUT HOW THE REQUEST INTERACTS WITH THIS
-    #AND SHARE WHAT THIS WILL HAVE TO BE STRUCTURED
+    ## params includes params["latitude"] which returns a STRING of the latitude, params ["longitude"] which reutrns a STRING of the longitude, and params["doNotWant"] which returns a STRING of whatever was specified in the form on the welcome page. It is not clear if we need to create strong params for this...
   end
 
   def show
 
   end
-
 
 end
