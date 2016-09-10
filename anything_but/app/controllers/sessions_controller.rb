83990d4dfddcb644352559f99314ddf083cb5c97
dillonmcroberts <dillon.mcroberts@gmail.com>
2016-09-10 15:11:32 -0400

    added user show page, navigation buttons to edit and delete user accounts

--- anything_but/app/controllers/sessions_controller.rb
+++ anything_but/app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_username(params[:username])
-
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    binding.pry
+    @user = User.find_by_username(params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       # redirect_to '/signin'
