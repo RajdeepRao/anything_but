a1dd9b530eaf237c8c19cd5e497e5b77d635149b
dillonmcroberts <dillon.mcroberts@gmail.com>
2016-09-11 17:11:38 -0400

    fix another conflict

--- anything_but/app/assets/javascripts/activity_autocomplete.js
+++ anything_but/app/assets/javascripts/activity_autocomplete.js
@@ -1,24 +1,5 @@
 $(function() {
-<<<<<<< HEAD
-    var availableTags = [
-        "ActionScript",         "AppleScript",
-        "Asp",              "BASIC",
-        "C",                "C++",
-        "Clojure",              "COBOL",
-        "ColdFusion",           "Erlang",
-        "Fortran",              "Groovy",
-        "Haskell",              "Java",
-        "JavaScript",           "Lisp",
-        "Perl",             "PHP",
-        "Python",               "Ruby",
-        "Scala",                "Scheme"
-    ];
-    $( "#do-not-want" ).autocomplete({
-        source: availableTags
-    });
-=======
   $("#do-not-want").autocomplete({
     source: 'activities',
   });
->>>>>>> 406e87a1a66eb75964da7871917cc3f1f24248e2
 });
