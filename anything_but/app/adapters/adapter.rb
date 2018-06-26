271c06dd968ac0744417244308e642d1a815cce0
Mahati <mahatigollamudi@gmail.com>
2018-06-26 16:38:42 -0400

    Update adapter.rb

--- anything_but/app/adapters/adapter.rb
+++ anything_but/app/adapters/adapter.rb
@@ -15,17 +15,19 @@ class Adapter
     end
 
 
-
+    #called in Recommendations Controller
     def initiate_api_req (latitude_num, longitude_num, categories)
       yelp_query_array = []
-      coordinates = {latitude: latitude_num, longitude: longitude_num}
+      coordinates = {latitude: latitude_num, longitude: longitude_num}  
       categories.each do |category|
         params = {category_filter: category}
         yelp_query_array << self.client.search_by_coordinates(coordinates, params).businesses
+        #push to empty array all the businesses from the Yelp API call that match the coordinates give and the category 
       end
-      yelp_query_array
+      yelp_query_array #all the businesses that are active, considered arts or nightlife that fall within the given coordinates
     end
 
+    #called in Recommendations Controller (doNotWant comes from params of what the user clicked for anything but x)
     def filter_api(recommendations, doNotWant)
       filter_array = []
       recommendations.each do |business|
