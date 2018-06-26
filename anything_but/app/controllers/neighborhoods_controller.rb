e3503d51d783bb327d5129d51b444d4c4f209d91
Mahati <mahatigollamudi@gmail.com>
2018-06-26 16:39:12 -0400

    Update neighborhoods_controller.rb

--- anything_but/app/controllers/neighborhoods_controller.rb
+++ anything_but/app/controllers/neighborhoods_controller.rb
@@ -3,8 +3,7 @@ class NeighborhoodsController < ApplicationController
 
   def create
     neighborhood = Neighborhood.create(:name => 'apiData.name')
-    #i have not parsed the AJAX well enough to know where/how the actual name of the
-    #neighborhood is being stored
+   
   end
 
 
